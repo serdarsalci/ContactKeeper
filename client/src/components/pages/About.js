fc227e630e321bf8533e9d8a7372fbb3af512cc7
serdar <salciserdar@yahoo.com>
2020-11-15 23:21:55 -0500

    front end started

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p className="my-1">
+        This is a full stack react App for keeping contacts
+      </p>
+      <p className="bg-dark p">
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </div>
+  )
+}
+export default About;
